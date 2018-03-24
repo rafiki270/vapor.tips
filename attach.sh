bc83b02cdd905e40d995fea951fcd3ed6e141c01
Ondrej Rafaj <ondrej.rafaj@gmail.com>
2018-03-24 09:52:11 +0000

    wip

---
+++ attach.sh
@@ -0,0 +1,1 @@
+sudo docker exec -i -t vapor.tips /bin/bash
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 docker stop vapor.tips
 docker rm vapor.tips
-docker run --name vapor.tips -p 3001:2368 -v /Projects/Web/vapor.tips:/var/lib/ghost/content ghost
+docker run -d --name vapor.tips -p 3001:2368 -v /Projects/Web/vapor.tips/data:/var/lib/ghost/content ghost
